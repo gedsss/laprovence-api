6ae39829aa9e62aa13b77cb6606f0cbd866074a1
Gabriel <gabrieledsoon3@gmail.com>
2026-04-30 21:29:52 -0400

    .

--- src/modules/listas/listas.schema.ts
+++ src/modules/listas/listas.schema.ts
@@ -4,10 +4,10 @@ export const CreateListasSchema = z.object({
   codigo: z.string().max(6, 'O código não deve ter mais de 6 caracteres').optional(),
   user_id: z.uuid(),
   nome_noivos: z.string(),
-  telefone: z.string().optional(),
-  data_casamento: z.coerce.date().optional(),
-  foto_casal: z.url().optional(),
-  mensagem_boas_vindas: z.string().optional(),
+  telefone: z.string().optional().nullable(),
+  data_casamento: z.coerce.date().optional().nullable(),
+  foto_casal: z.string().optional().nullable(),
+  mensagem_boas_vindas: z.string().optional().nullable(),
   status: z.enum(['Ativa', 'Arquivada']).default('Ativa'),
 })
 
@@ -21,10 +21,10 @@ export const UpdateListasSchema = z.object({
     .max(6, 'O código não deve ter mais de 6 caracteres')
     .optional(),
   nome_noivos: z.string().optional(),
-  telefone: z.string().optional(),
-  data_casamento: z.coerce.date().optional(),
-  foto_casal: z.url().optional(),
-  mensagem_boas_vindas: z.string().optional(),
+  telefone: z.string().optional().nullable(),
+  data_casamento: z.coerce.date().optional().nullable(),
+  foto_casal: z.string().optional().nullable(),
+  mensagem_boas_vindas: z.string().optional().nullable(),
   status: z.enum(['Ativa', 'Arquivada']).default('Ativa').optional(),
 })
 
